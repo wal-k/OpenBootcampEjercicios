4caea19163ecbe88c2aa1a6a18deb6e327db69ca
wal-k <89323977+wal-k@users.noreply.github.com>
2023-03-28 13:08:52 -0300

    JavaInicial EjerciciosTema1

---
+++ JavaInicial/src/EjerciciosTema1/ClaseDatos.java
@@ -0,0 +1,29 @@
+package EjerciciosTema1;
+
+public class ClaseDatos {
+    static byte varByte = 127;
+    static short varShort = 32767;
+    static int varInt = 2147483647;
+    static long varLong = 9223372036854775806L;
+    static float varFloat = 0.123456f;
+    static double varDouble = 0.123456789012345;
+    static boolean varBool = true;
+    static char varChar = 'C';
+    static String varString = "String12345567890";
+
+
+    public static void main(String[] args) {
+
+        System.out.println(varByte);
+        System.out.println(varShort);
+        System.out.println(varInt);
+        System.out.println(varLong);
+        System.out.println(varFloat);
+        System.out.println(varDouble);
+        System.out.println(varBool);
+        System.out.println(varChar);
+        System.out.println(varString);
+        
+    }
+
+}
